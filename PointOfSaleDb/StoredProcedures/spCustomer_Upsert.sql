73b4b19f26c6ab47904438e4c01950f0dfe72390
Michael Shepherd M <shepherd.munemo@outlook.com>
2023-09-12 09:45:38 +0200

    Creating Database & stored Procs

---
+++ PointOfSaleDb/StoredProcedures/spCustomer_Upsert.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[spCustomer_Upsert]
+	@CustomerId int output,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@Email nvarchar(50)
+AS
+	BEGIN
+		IF EXISTS (SELECT 1 FROM Customer WHERE Email = @Email)
+		BEGIN
+			UPDATE Customer SET
+				FirstName = @FirstName,
+				LastName = @LastName
+			WHERE Email = @Email;
+			SELECT @CustomerId = Id FROM Customer WHERE Email = @Email;
+		END
+
+		ELSE
+		BEGIN
+			INSERT INTO Customer (FirstName, LastName, Email)
+			VALUES (@FirstName, @LastName, @Email);
+			SELECT @CustomerId = SCOPE_IDENTITY();
+		END
+
+	END
---
+++ PointOfSaleDb/StoredProcedures/spPurchase_Insert.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spPurchase_Insert]
+	@Item nvarchar(50),
+	@Cost money,
+	@CustomerId int
+AS
+	BEGIN
+		INSERT INTO Purchase (Item, Cost, CustomerId)
+		VALUES (@Item, @Cost, @CustomerId);
+	END
